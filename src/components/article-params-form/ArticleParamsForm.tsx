c58fa6f86fd18fe589b793ea9cf2cb34172011db
Alexey Gustav <gustavaleksej9@gmail.com>
2025-03-20 00:17:09 +0300

    add useState

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [active, setActive] = useState(false);
+
+	const toggle = () => {
+		if (active) {
+			setActive(false); // Закрываем меню, если оно открыто
+		} else {
+			setActive(true); // Открываем меню, если оно закрыто
+		}
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
+			<ArrowButton isOpen={active} onClick={toggle} />
 			<aside className={styles.container}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
